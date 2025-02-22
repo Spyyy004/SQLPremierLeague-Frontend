370385ea9b4182cea6100ce68ef7a3999041834b
AyushPawar004 <ayush.pawar@mosaicwellness.in>
2025-02-22 18:42:04 +0530

    config changes

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base:'/challenges',
   server: {
     port: 3000, // 👈 Set the port to 3000
   }
